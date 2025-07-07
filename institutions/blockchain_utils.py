80a4eedc3c5f38c612289a073e699a2d23f90118
Abdullo200604 <lwcardinal12@gmail.com>
2025-07-07 15:27:27 +0500

    Blockchain, QR, env example qo‘shildi

--- institutions/blockchain_utils.py
+++ institutions/blockchain_utils.py
@@ -1,7 +1,8 @@
+import os
 from web3 import Web3
 from dotenv import load_dotenv
-import os
 
+# .env fayldan konfiguratsiya o‘qiladi
 load_dotenv()
 
 INFURA_URL = os.getenv('INFURA_URL')
@@ -34,16 +35,33 @@ contract_abi = [
     }
 ]
 
+# Web3 ulanishi
 w3 = Web3(Web3.HTTPProvider(INFURA_URL))
 contract = w3.eth.contract(address=CONTRACT_ADDRESS, abi=contract_abi)
 
 def save_hash_to_blockchain(hash_value):
-    tx = contract.functions.addDocumentHash(hash_value).build_transaction({
+    """
+    Hujjat hashini (sha256, hex formatda) Ethereum blokcheynga yozadi.
+    :param hash_value: 64 belgili hex string (misol: "740d13b3...")
+    :return: tx_hash (string)
+    """
+    # 1. Hex stringni bytes32 formatga o‘girish
+    if not hash_value.startswith('0x'):
+        doc_hash = Web3.to_bytes(hexstr='0x' + hash_value)
+    else:
+        doc_hash = Web3.to_bytes(hexstr=hash_value)
+
+    # 2. Tranzaksiyani qurish
+    tx = contract.functions.addDocumentHash(doc_hash).build_transaction({
         'from': WALLET_ADDRESS,
         'nonce': w3.eth.get_transaction_count(WALLET_ADDRESS),
         'gas': 300000,
         'gasPrice': w3.to_wei('20', 'gwei')
     })
+
+    # 3. Tranzaksiyani imzolash
     signed_tx = w3.eth.account.sign_transaction(tx, PRIVATE_KEY)
-    tx_hash = w3.eth.send_raw_transaction(signed_tx.rawTransaction)
+
+    # 4. Yangi versiyada: raw_transaction
+    tx_hash = w3.eth.send_raw_transaction(signed_tx.raw_transaction)
     return tx_hash.hex()
--- institutions/signals.py
+++ institutions/signals.py
@@ -1,14 +1,18 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from .models import Certificate
+from .utils import generate_qr
 from .blockchain_utils import save_hash_to_blockchain
 
 @receiver(post_save, sender=Certificate)
-def write_hash_to_blockchain(sender, instance, created, **kwargs):
+def certificate_post_save(sender, instance, created, **kwargs):
+    # Faqat yangi Certificate yaratilganda ishlaydi
     if created:
-        tx_hash = save_hash_to_blockchain(
-            instance.hash,
-            from_address='0x...',       # ETH wallet manzili
-            private_key='PRIVATE_KEY'   # ETH wallet private key
-        )
-        # Istasangiz, tx_hash ni modelga yoki logga yozing
+        # 1. Blockchain'ga hash yozish (hash_value string)
+        tx_hash = save_hash_to_blockchain(instance.hash)
+        # 2. QR-code generatsiya qilish (hash asosida)
+        qr_file = generate_qr(instance.hash)
+        # QR-code'ni fayl sifatida saqlash
+        instance.qr_code.save(f"{instance.pk}_qr.png", qr_file, save=False)
+        # O'zgarishlarni saqlash (loopdan qochish uchun save=False yuqorida)
+        instance.save()
